29bb6b9cfd2a729ea5af3fcece42d381c14d5d77
Lucas <lucas.santos@tallos.com.br>
2022-07-13 10:00:35 -0300

    style: Finalizando estilizacao da tabela

--- src/views/funcionarios-lista/funcionarios-lista.vue
+++ src/views/funcionarios-lista/funcionarios-lista.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="main">
     <Menu />
-    <div class="tabela">
+    <div>
       <b-table
+        class="tabela"
         striped
-        bordered
+        borderless
         responsive
         hover
+        fixed
         :items="usuarios"
         :fields="filds"
       >
+        <template #cell(name)="data">
+          <b-card-text class="info">{{ data.value }}</b-card-text>
+        </template>
+        <template #cell(email)="data">
+          <b-card-text class="info">{{ data.value }}</b-card-text>
+        </template>
+        <template #cell(role)="data">
+          <b-card-text class="info">{{ data.value }}</b-card-text>
+        </template>
       </b-table>
     </div>
   </div>
@@ -43,7 +54,8 @@ export default {
     this.$http
       .get("list-all")
       .then((response) => (this.usuarios = response.data))
-      .catch((erro) => console.log(erro));
+      .catch((erro) => console.log(erro))
+      .finally(() => console.log(this.usuarios));
   },
 
   components: {
@@ -57,14 +69,14 @@ export default {
   width: 100vw;
   background-color: #110729;
 }
-table {
-  color: #110729;
-}
 .tabela {
-  width: 90%;
+  width: 60%;
   margin-top: 3%;
-  margin-left: 5%;
+  margin-left: 20%;
   background-color: #dcdcdc;
-  border-radius: 10px;
+  border-radius: 15px;
+}
+.info {
+  color: #110729;
 }
 </style>
